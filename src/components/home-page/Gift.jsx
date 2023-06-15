9508c8328b6509b4d33530b39c161391ff3c092d
Brian <linbrian24@gmail.com>
2023-06-14 22:55:36 -0400

    Style Gift and adjust padding issues (mobile)

--- src/components/home-page/Gift.jsx
+++ src/components/home-page/Gift.jsx
@@ -1,15 +1,16 @@
 export default function Gift() {
   return (
-    <div className="flex p-32 pt-0">
-      {/* Image */}
-      <div className="w-1/2 border"></div>
-      <div className="border w-1/2  pl-8 py-32 flex flex-col justify-center items-start gap-4">
-        <h1 className="text-4xl font-karla font-bold">The Best Gift</h1>
-        <p>
-          Delight your friends, family, or lover with an H2O Nail & Spa gift
-          card! Gift cards can be purchased in person at our salon.
-        </p>
-      </div>
+    <div className="flex flex-col text-center gap-4 mx-8 my-40">
+      <img
+        className="border-2 border-black rounded"
+        src="./images/giftcard.jpg"
+        alt="gift-card"
+      />
+      <h1 className="text-3xl font-karla font-bold">The Best Gift</h1>
+      <p className="mx-4">
+        Delight your friends, family, or lover with an H2O Nail & Spa gift card!
+        Gift cards can be purchased in person at our salon.
+      </p>
     </div>
   );
 }
--- src/components/home-page/Services.jsx
+++ src/components/home-page/Services.jsx
@@ -33,7 +33,7 @@ export default function Services() {
   ];
 
   return (
-    <div className="flex flex-col items-center mt-20 gap-8 font-karla text-center">
+    <div className="flex flex-col items-center mt-20 mb-40 gap-8 font-karla text-center">
       <h1 className="text-3xl font-bold">Services</h1>
       <li className="grid gap-6">
         {services.map((service) => {
