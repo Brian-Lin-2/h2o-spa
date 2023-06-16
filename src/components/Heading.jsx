ea7b0d71e9c0f2b16cc5f14681179d3ed2c388cc
Brian <linbrian24@gmail.com>
2023-06-15 22:39:05 -0400

    Style Header and Heading (desktop)

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,6 +1,6 @@
 export default function Heading() {
   return (
-    <div className="flex justify-between items-center p-6 bg-blue-1 lg:px-32">
+    <div className="flex justify-between items-center p-6 bg-blue-1 lg:px-32 lg:pt-8">
       <img className="w-20" src="./images/logo.png" />
       <img className="w-10 lg:hidden" src="./images/hamburger-icon.png" />
       <h1 className="hidden gap-6 font-karla font-bold items-center tracking-wide lg:flex">
--- src/components/home-page/Header.jsx
+++ src/components/home-page/Header.jsx
@@ -4,12 +4,12 @@ export default function Header() {
   return (
     <>
       <Heading />
-      <div className="flex flex-col-reverse bg-blue-1 p-6 pb-12 lg:flex-row lg:px-40 lg:gap-8 lg:pt-12 lg:pb-32 lg:items-center border">
-        <div className="border text-center mt-8">
-          <h1 className="text-4xl font-bold mb-4 font-karla">
+      <div className="flex flex-col-reverse bg-blue-1 p-6 pb-12 lg:flex-row lg:px-40 lg:gap-8 lg:pt-12 lg:pb-32 lg:items-center">
+        <div className="text-center mt-8 lg:mx-20 lg:text-start">
+          <h1 className="text-4xl font-bold mb-4 font-karla lg:text-6xl">
             Nails You Will Love!
           </h1>
-          <p className="font-verdana text-dark-gray">
+          <p className="font-verdana text-dark-gray mx-2 lg:ml-0 lg:mr-20 lg:text-lg">
             Providing nail and spa services with the upmost care and
             cleanliness.
           </p>
