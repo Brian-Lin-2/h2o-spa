c18078c657e5fe2066dd302bbca0f02a88d04462
Brian <linbrian24@gmail.com>
2023-06-20 11:19:00 -0400

    Reformat Service Navbar

--- src/components/services-page/Navbar.jsx
+++ src/components/services-page/Navbar.jsx
@@ -11,18 +11,59 @@ import { PropTypes } from "prop-types";
 export default function Navbar({ initial }) {
   const [active, setActive] = useState(initial);
 
-  function handleClick(service) {
-    setActive(service);
-  }
-
   return (
-    <div className=" mt-16 mb-32 mx-12 lg:mx-32">
-      <h1 className="text-4xl text-center font-karla font-bold mb-12">
-        Services
-      </h1>
+    <div className="">
+      <div className="bg-blue-2 font-bold">
+        <h1 className="text-4xl text-center font-karla py-12">Services</h1>
 
-      <div className="flex flex-col-reverse">
-        <div className="">
+        <div className="grid gap-4 grid-cols-2 font-karla text-center pb-20 px-12 lg:grid-cols-3 lg:px-96 lg:gap-6">
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => setActive("Manicure")}
+          >
+            Manicure
+          </a>
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => setActive("Pedicure")}
+          >
+            Pedicure
+          </a>
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => setActive("Massage")}
+          >
+            Massage
+          </a>
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => setActive("Facial")}
+          >
+            Facial
+          </a>
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => setActive("Waxing")}
+          >
+            Waxing
+          </a>
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
+            onClick={() => setActive("Spa Special")}
+          >
+            Spa Special
+          </a>
+          <a
+            className="border border-black p-2 rounded-full cursor-pointer relative left-1/2 lg:col-start-2 lg:left-0 hover:text-white hover:bg-black"
+            onClick={() => setActive("Kid's Menu")}
+          >
+            Kid's Menu
+          </a>
+        </div>
+      </div>
+
+      {active && (
+        <div className="p-8 lg:px-40">
           {active === "Manicure" && <Manicure />}
           {active === "Pedicure" && <Pedicure />}
           {active === "Massage" && <Massage />}
@@ -31,52 +72,7 @@ export default function Navbar({ initial }) {
           {active === "Spa Special" && <Special />}
           {active === "Kid's Menu" && <KidMenu />}
         </div>
-
-        <div className="grid gap-4 grid-cols-2 font-karla text-center mb-20 lg:grid-cols-3 lg:mx-48 lg:gap-6">
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
-            onClick={() => setActive("Manicure")}
-          >
-            Manicure
-          </a>
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
-            onClick={() => setActive("Pedicure")}
-          >
-            Pedicure
-          </a>
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
-            onClick={() => setActive("Massage")}
-          >
-            Massage
-          </a>
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
-            onClick={() => setActive("Facial")}
-          >
-            Facial
-          </a>
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
-            onClick={() => setActive("Waxing")}
-          >
-            Waxing
-          </a>
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer hover:text-white hover:bg-black"
-            onClick={() => setActive("Spa Special")}
-          >
-            Spa Special
-          </a>
-          <a
-            className="border border-black font-bold p-2 rounded-full cursor-pointer relative left-1/2 lg:col-start-2 lg:left-0 hover:text-white hover:bg-black"
-            onClick={() => setActive("Kid's Menu")}
-          >
-            Kid's Menu
-          </a>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
