742e3f536490f7667a4178303411b0e880dd0214
Brian <linbrian24@gmail.com>
2023-06-27 13:10:26 -0400

    Make InfoPopUp repsonsive

--- src/components/InfoPopUp.jsx
+++ src/components/InfoPopUp.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 export default function InfoPopUp({ setClick }) {
   return (
     <div className="border fixed inset-0 bg-dark-gray-transparent">
-      <div className="flex flex-col bg-white items-center my-36 mx-28 md:flex-row md:my-32 md:mx-72 shadow-2xl">
+      <div className="flex flex-col bg-white items-center my-48 mx-[22vw] md:flex-row md:my-32 md:mx-[20vw] shadow-2xl">
         <img
           className="md:w-7/12"
           src="./images/person-calling.jpg"
@@ -12,16 +12,16 @@ export default function InfoPopUp({ setClick }) {
 
         <a onClick={() => setClick(false)}>
           <img
-            className="w-6 relative bottom-52 left-28 md:w-12 md:bottom-40 md:left-76 hover:cursor-pointer"
+            className="w-[5vw] relative bottom-[40vw] left-[22vw] md:w-[3.25vw] md:bottom-[11vw] md:left-[21vw] hover:cursor-pointer"
             src="./images/close-icon.svg"
           ></img>
         </a>
 
         <div className="mb-14 mt-4 mx-6 text-center md:text-start md:m-0 md:ml-2 md:mr-16">
-          <h1 className="font-karla font-bold text-3xl mb-1 md:text-4xl md:mb-2">
+          <h1 className="font-karla font-bold text-3xl leading-none mb-1 md:text-[2.5vw] md:leading-none md:mb-2">
             Call Today!
           </h1>
-          <p className="font-verdana text-sm text-dark-gray md:text-base">
+          <p className="font-verdana text-sm text-dark-gray md:text-[1.1vw] md:leading-[1.5vw] md:ml-1">
             Please call us at 516-766-2576 to reserve an appointment!
           </p>
         </div>
