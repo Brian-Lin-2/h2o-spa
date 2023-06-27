92b1d5a639467fe27b8bdc177116bd1a610acfc2
Brian <linbrian24@gmail.com>
2023-06-26 22:52:41 -0400

    Make header responsive

--- src/components/home-page/Header.jsx
+++ src/components/home-page/Header.jsx
@@ -25,16 +25,14 @@ export default function Header({ setPage }) {
 
       <div>
         <Heading setPage={setPage} isHome={true} />
-        <div className="flex mt-8 mb-92 md:flex-row md:mt-0 md:gap-8 md:pt-12 md:w-1/2 md:items-center">
-          <div className="text-center mt-2 md:ml-48 md:text-start">
-            <h1 className="text-5xl mx-12 font-bold mb-1 font-karla md:text-8xl md:-tracking-wide">
-              Nails You Will Love!
-            </h1>
-            <p className="font-verdana text-sm text-dark-gray mx-16 md:mr-20 md:text-lg">
-              Providing nail and spa services with the upmost care and
-              cleanliness.
-            </p>
-          </div>
+        <div className="flex flex-col mt-8 mb-92 text-center -tracking-wide md:mt-0 md:ml-[15vw] md:text-start md:w-1/2 border-red-500 md:items-center lg:mt-[5vw]">
+          <h1 className="text-6xl mx-[12vw] font-bold mb-1 font-karla md:ml-12 md:text-[6vw]">
+            Nails You Will Love!
+          </h1>
+          <p className="font-verdana text-sm text-dark-gray mx-[16vw] md:ml-16 md:mr-[20vw] md:text-[1.3vw] md:leading-[2vw]">
+            Providing nail and spa services with the upmost care and
+            cleanliness.
+          </p>
         </div>
       </div>
     </>
